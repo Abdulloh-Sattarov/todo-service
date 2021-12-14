bb795ae0cbf12b172ae342269db0320fd001b1f1
Abdulloh <developer15pro@gmail.com>
2021-12-14 23:21:21 +0500

    Todo Service

--- migrations/000001_initial_migration.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS todos(
-    id SERIAL Primary Key,
-    assignee VARCHAR(50),
-    title VARCHAR(50),
-    summary VARCHAR(50),
-    deadline  timestamp not null,
-    todo_status VARCHAR(50)
-);
